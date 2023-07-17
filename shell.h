0eec52de320a7f1b18525486c79e3dc89c75514e
phosecode <victorolamy@gmail.com>
2023-07-17 07:12:13 +0100

    Create shell.h header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+
+#endif /** SHELL **/
