491fd2b2aa5c9d779abcc575496d8104474a1edf
trucedev <ogunniranolaoluwa12@gmail.com>
2023-07-18 21:14:06 +0100

    update shell.h file

--- shell.h
+++ shell.h
@@ -13,4 +13,36 @@
 #include <sys/type.h>
 #include <sys/wait.h>
 
+/**
+  * struct shell_env - stores address for pointer to free
+  * @path_values: pointer to the path values of cmd
+  * @input: pointer to buffer that get lines create upon run
+  * @input_token: pointer for input tokens
+  * @modify_path: pointer to the path after it has been modified
+  *
+  * Description: provide a storage for all elements that are
+  * allocated in the memory
+  */
+typedef struct shell_env
+{
+	char **path_values;
+	char *input;
+	char **input_token;
+	char *modify_path;
+} shell_t;
+
+/**
+  * struct built-in-cmd - structure for different built-in commands
+  * @cmd_name: name of commands
+  * @cmd: function pointer to run the command
+  *
+  * Description: structure for different built in command in our shell
+  */
+typedef struct built-in-cmd
+{
+	char *cmd_name;
+	void (*cmd)(shell_t *);
+} built_t;
+
+
 #endif /** SHELL **/
