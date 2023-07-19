e550e7cf8a12ae168b9412a640d595b755fb8a2d
Pontsho04 <mabopontsho@gmail.com>
2023-07-19 05:12:20 -0700

    exit_shell.c

---
+++ env_shell.c
@@ -0,0 +1,8 @@
+#include "shell.h"
+
+/**
+ * _myenv - to print current environment
+ * @info: contains potential arguments used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
