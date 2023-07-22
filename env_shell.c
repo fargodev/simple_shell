438711d5bcc605b94463fac2f78112c3d576db52
Pontsho04 <mabopontsho@gmail.com>
2023-07-22 04:38:44 -0700

    Updated

--- env_shell.c
+++ env_shell.c
@@ -19,7 +19,6 @@ int _myenv(info_t *info)
  * @name: env name
  * Return: the value
  */
-
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
@@ -42,7 +41,6 @@ char *_getenv(info_t *info, const char *name)
  * constant function prototype.
  * Return: Always 0
  */
-
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
@@ -61,7 +59,6 @@ int _mysetenv(info_t *info)
  * constant function prototype.
  * Return: Always 0
  */
-
 int _myunsetenv(info_t *info)
 {
 	int i;
@@ -72,7 +69,7 @@ int _myunsetenv(info_t *info)
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[1]);
+		_setenv(info, info->argv[1]);
 
 	return (0);
 }
@@ -83,7 +80,6 @@ int _myunsetenv(info_t *info)
  * constant function prototype.
  * Return: Always 0
  */
-
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
