bb799b2c59a2ce8243dc5f7bbda2dd46d34fac6e
trucedev <ogunniranolaoluwa12@gmail.com>
2023-07-25 16:47:53 +0100

    Update strtoken.c handling conversion of cmd to tokens

--- string.c
+++ string.c
@@ -52,7 +52,7 @@ int _strcmp(char *str1, char *str2)
 	while (*str1 != '\0' || *str2 != '\0')
 	{
 		if (*str1++ != *str2++)
-			return (1)
+			return (1);
 	}
 	if (*str1 == '\0' && *str2 == '\0')
 		return (0);
@@ -68,9 +68,34 @@ int _strcmp(char *str1, char *str2)
   */
 char **tokenize(char *str, char *delim)
 {
-	char **str_dup, **tokens, *tok;
+	char *str_dup, **tokens, *tok;
+	size_t delim_num = 0;
 
 	if (!str || !delim)
 		return (NULL);
 	str_dup = _strdup(str);
 
+	/** delimiter counter **/
+	tok = _strtok(str_dup, delim);
+	while (tok)
+	{
+		delim_num++;
+		tok = _strtok(NULL, delim);
+	}
+	free(str_dup);
+
+	tokens = malloc(sizeof(char *) * (delim_num + 1));
+	if (!tokens)
+		return (NULL);
+	/** tokenize string **/
+	tok = _strtok(str, delim);
+	delim_num = 0;
+	while (tok)
+	{
+		tokens[delim_num] = tok;
+		tok = _strtok(NULL, delim);
+		delim_num++;
+	}
+	tokens[delim_num] = NULL;
+	return (tokens);
+}
--- strtoken.c
+++ strtoken.c
@@ -21,4 +21,29 @@ char *_strtok(char *str, const char *delim)
 	}
 	else
 		str_end = str + _strlen(str);
-	for (i = 0; str + i < str_end; 
+	for (i = 0; str + i < str_end; i++)
+	{
+		for (j = 0; delim != NULL && delim[j] != '\0'; j++)
+		{
+			if (str[i] == delim[j])
+			{
+				if (word_found == 1)
+				{
+					str[i] = '\0';
+					end = str + i;
+					return (str_begin);
+				}
+				break;
+			}
+		}
+		if (delim[j] == '\0' && word_found == 0)
+		{
+			word_found = 1;
+			str_begin = str + i;
+		}
+	}
+	end = NULL;
+	if (word_found == 1)
+		return (str_begin);
+	return (NULL);
+}
