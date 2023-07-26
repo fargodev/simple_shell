ba91f909572c9e817740dbd9716379fe2097c6df
Pontsho04 <mabopontsho@gmail.com>
2023-07-26 08:01:42 -0700

    Updated

--- header.h
+++ header.h
@@ -16,6 +16,68 @@
 #include <limits.h>
 
 /**
+ * struct liststr - singly linked list
+ * @str: string
+ * @num: number field
+ * @next: next node
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - has pseudo-arguements to pass into function,
+ * allowing uniform prototype for function pointer struct
+ * @arg: string generated from getline containing arguements
+ * @argv: an array of strings generated from arg
+ * @argc: argument count
+ * @path: string path for current command
+ * @line_count: error count
+ * @err_num: error code for exit()s
+ * @linecount_flag: if on count this lineof input
+ * @fname: program file name
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @history: history node
+ * @histcount: history line number count
+ * @alias: alias node
+ * @readfd: fd from which to read line input
+ * @env_changed: on if environ changed
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @status: return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory management */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
   * struct shell_env - store shell environment addresses
   * @path_values: pointer to values of the shell path
   * @input: input token
@@ -74,9 +136,6 @@ void free_shell(shell_t *);
 void print_cmd(char *, char *);
 
 /** builtin.c **/
-int _myexit(info_t);
-int _mycd(info_t);
-int _myhelp(info_t);
 void made_exit(shell_t *);
 void print_env(shell_t *);
 
